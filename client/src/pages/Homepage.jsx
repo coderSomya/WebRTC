3288753fb527f31bb2956c0d7c031d1d73f4fff8
Somya <isomya13@gmail.com>
2023-08-28 17:51:35 +0530

    [bug]: emai, room undefined

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import './Homepage.css'
 import { useSocket } from "../providers/Socket";
 
 
 const Homepage = ()=>{
 
+    const [email, setEmail] = useState();
+    const [room, setRoom] = useState();
+
    const {socket} = useSocket();
-   socket.emit('join-room', {roomid:1, emailid: "a@b.com"});
+   const handleJoinRoom= ()=>{
+      console.log("user", email, "joined in room", room)
+      socket.emit("join-room", {email, room});
+   }
    return(
     <>
    <div className="homepage-container">
     <div>
-        <input type="text" placeholder="enter your email here"/>
-        <input type="text" placeholder="enter room code"/>
-        <button>GO</button>
+        <input value={email}
+        onChange={(e)=>setEmail(e.target.value)} type="text" placeholder="enter your email here"/>
+        <input value={room} 
+        onChange={(e)=>setRoom(e.target.value)}
+        type="text" placeholder="enter room code"/>
+        <button onClick={handleJoinRoom}>GO</button>
         </div>
     </div> 
     </>
--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const {Server} = require('socket.io')
+const {Server} = require('socket.io')
 // const cors= require('cors')
 
-// const io = new Server();
+const io = new Server({
+    cors:true
+});
 const app = express();
-const http = require("http").createServer(app);
-const io=require('socket.io')(http, {
-    cors: {
-        origin:"*"
-    }
-})
+// const http = require("http").createServer(app);
+// const io=require('socket.io')(http, {
+//     cors: {
+//         origin:"*"
+//     }
+// })
 
 const port1 =8000;
 const port2= 8001;
